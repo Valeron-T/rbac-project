0d5b30c9b90c5853a068479b9fe1b85043108d6f
Valeron <32789691+Valeron-T@users.noreply.github.com>
2024-12-09 00:00:06 +0530

    added permission management apis (#3)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 
 from routes.users import router as users_router
 from routes.roles import router as roles_router
+from routes.permissions import router as permissions_router
 
 load_dotenv()
 
@@ -13,6 +14,7 @@ SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
 app = FastAPI()
 app.include_router(users_router, prefix="/users", tags=["Users"])
 app.include_router(roles_router, prefix="/roles", tags=["Roles"])
+app.include_router(permissions_router, prefix="/permissions", tags=["Permissions"])
 
 
 @app.get("/")
